1ac0c8a865bf7fb537f4ed3ce28624b1b87f2afb
Sebastian Cifuentes Castro <sebastian@Mac.lan>
2025-05-09 19:44:36 -0500

    L

--- routes/cursos.js
+++ routes/cursos.js
@@ -15,7 +15,7 @@ const generarSlug = (nombre) =>
 // 📌 Obtener todos los cursos con cantidad de inscritos
 router.get('/con-inscritos', async (req, res) => {
   try {
-    const cursos = await Curso.find();
+    const cursos = await Curso.find().select('+horarios +horario');
 
     const conteo = await Inscripcion.aggregate([
       { $group: { _id: "$cursoId", total: { $sum: 1 } } }
